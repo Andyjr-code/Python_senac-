b429688e18e6c1c0f76a6e898fd4d4eef0f96233
Andyjr-code <andyjrblanco01@gmail.com>
2025-10-08 21:32:32 -0300

    lista2

---
+++ Listas/Lista_2/algorismo38.py
@@ -0,0 +1,5 @@
+""" Ler um número real e imprimir a parte terça deste número """
+num = float(input("Digite um número real: "))
+
+parte_terca = num  / 3
+print("A pate terca é: ", parte_terca)
---
+++ Listas/Lista_2/algorismo39.py
@@ -0,0 +1,7 @@
+""" Entra com dois números reais e imprimir a média aritmética com a
+mensagem “média” """
+
+num1 = float(input("Digite um número: "))
+num2 = float(input("Digite outro número:"))
+media = (num1 + num2) / 2
+print("A média é: ", media) 
